6d9ed70b66d6ce56864c4497b96ece80cda84656
Gabriel Xavier <100116691+0nlySpirit@users.noreply.github.com>
2026-03-24 23:15:55 -0400

    changes:: improves in Cargo.toml and Docs

--- src/cli/commands.rs
+++ src/cli/commands.rs
@@ -6,21 +6,23 @@ use clap::Subcommand;
 pub enum Commands {
 
 
-    /// Init the `Emet::up` with the `private_key` provided (WARNING: Don't forget your `private_key`)
+    /// Init the `Emet::up`
     Up {
+        /// Your `private_key`
         private_key: Option<String>,
         
+        /// To show the `private_key` defined
         #[arg(short, long)]
         show: bool
     },
 
-    /// Seal the `path` provided with `Emet::up().seal()`
+    /// Seal the `path` provided with `Emet::up().seal()` and save the file signed in a `.emet` file
     Seal {
         path: Option<PathBuf>
     },
 
 
-    /// Check if `original file` and `.emet` encrypted file have your hashes matching
+    /// Check if `original file` and `.emet` file signed is authentic
     Check {
         path: Option<PathBuf>,
         emet_path: Option<PathBuf>
